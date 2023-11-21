22436a8819db6795c23aa3e3b0a9a35af3c9819a
shihab shumon <shumonshihab62@gmail.com>
2023-11-21 21:18:50 +0600

    solved Absolute and StringToUnion

--- challenges/Medium/AppendToObject.ts
+++ challenges/Medium/AppendToObject.ts
@@ -1,3 +1,16 @@
 type AppendToObject<T, U extends PropertyKey, V> = {
   [P in keyof T | U]: (T & Record<U, V>)[P]
 }
+
+// alternative but easiest
+
+// type AppendToObject<T, U extends PropertyKey, V> = {
+//   [P in keyof T | U]: P extends keyof T ? T[P] : V
+// }
+
+// another alternative
+
+// type Flatten<T> = {
+//   [K in keyof T]: T[K]
+// }
+// type AppendToObject<T, U extends PropertyKey, V> = Flatten<T & { [K in U]: V }>
---
+++ challenges/Medium/StringToUnion.ts
@@ -0,0 +1,3 @@
+type StringToUnion<T extends string> = T extends `${infer Head}${infer Tail}`
+  ? Head | StringToUnion<Tail>
+  : never
